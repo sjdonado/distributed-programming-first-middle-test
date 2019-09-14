6b515de1266ddb1013e944d1fac78600b4dfbce1
RnJohn <jefopa@hotmail.com>
2019-09-14 02:12:25 -0500

    Fixed multicast communication

--- TCPManager/src/main/java/com/mycompany/tcpmanager/TCPClientManager.java
+++ TCPManager/src/main/java/com/mycompany/tcpmanager/TCPClientManager.java
@@ -157,12 +157,12 @@ public class TCPClientManager extends Thread {
                                     );
                                 }
                             } else {
-                                byte[] offset = binaryCounter(counter, remainingBytes, this.clientManagerId);
-                                chunk[1496] = offset[0];
-                                chunk[1497] = offset[1];
-                                chunk[1498] = offset[2];
-                                chunk[1499] = offset[4];
-                                
+//                                byte[] offset = binaryCounter(counter, remainingBytes, this.clientManagerId);
+//                                chunk[1496] = offset[0];
+//                                chunk[1497] = offset[1];
+//                                chunk[1498] = offset[2];
+//                                chunk[1499] = offset[4];
+//                                
                                 this.caller.chunkReceivedFromClient(clientSocket, chunk);
                                 counter++;
 //                                if (remainingBytes == 0) sendMessage(new byte[] {0, 1});
@@ -223,18 +223,20 @@ public class TCPClientManager extends Thread {
         }
     }
     
-    public byte[] binaryCounter(int counter, int remainingBytes, int socketID){
-        byte[] offset = new byte[4];
-        
-        offset[0] = Byte.parseByte(Integer.toBinaryString(counter % 255),2);
-        offset[1] = Byte.parseByte(Integer.toBinaryString(counter / 255),2);
-        offset[2] = Byte.parseByte(Integer.toBinaryString(counter / 65535),2);
-        int bit25th = counter/16777215;
-        
-        
-        String id = Integer.toBinaryString(socketID).substring(2, 8);
-        offset[4] = Byte.parseByte((remainingBytes == 0 ? 1 : 0) + id + bit25th);
-        
-        return offset;
-    }
+//    public byte[] binaryCounter(int counter, int remainingBytes, int socketID){
+//        byte[] offset = new byte[4];
+//        
+//        offset[0] = Byte.parseByte(Integer.toBinaryString(counter % 255),2);
+//        offset[1] = Byte.parseByte(Integer.toBinaryString(counter / 255),2);
+//        offset[2] = Byte.parseByte(Integer.toBinaryString(counter / 65535),2);
+//        int bit25th = counter/16777215;
+//        
+//        System.out.println(Integer.toBinaryString(socketID));
+//        String id2 = Integer.toBinaryString(socketID);
+//        
+//        String id = Integer.toBinaryString(socketID).substring(2, 6);
+//        offset[4] = Byte.parseByte((remainingBytes == 0 ? 1 : 0) + id + bit25th);
+//        
+//        return offset;
+//    }
 }
--- UDPReceptor/src/main/java/com/mycompany/udpmanager/UDPManager.java
+++ UDPReceptor/src/main/java/com/mycompany/udpmanager/UDPManager.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.SocketException;
 
 /**
@@ -16,8 +17,10 @@ import java.net.SocketException;
  * @author sjdonado
  */
 public class UDPManager extends Thread {
+    
+    MulticastSocket multicastSocket;
     static int receptorIdCounter = 0;
-    private DatagramSocket datagramSocket;
+    //private DatagramSocket datagramSocket;
     private int listeningPort;
     private String defaultIpAddress;
     private UDPManagerCallerInterface caller;
@@ -35,9 +38,11 @@ public class UDPManager extends Thread {
 
     private boolean initializeDatagramSocket() {
         try {
-            this.datagramSocket = new DatagramSocket(listeningPort);
+            this.multicastSocket = new MulticastSocket(listeningPort);
+            InetAddress inetAddress = InetAddress.getByName(defaultIpAddress);
+            multicastSocket.joinGroup(inetAddress);
             return true;
-        } catch(SocketException error) {
+        } catch(Exception error) {
             this.caller.exceptionHasBeenThrown(error);
         }
         return false;
@@ -49,7 +54,7 @@ public class UDPManager extends Thread {
             DatagramPacket datagramPacket = new DatagramPacket(new byte[1500], 1500);
             if (initializeDatagramSocket()) {
                 while (this.isEnabled) {
-                    datagramSocket.receive(datagramPacket);
+                    multicastSocket.receive(datagramPacket);
                     this.caller.dataReceived(
                         this.receptorId,
                         datagramPacket.getAddress().toString(),
@@ -68,7 +73,7 @@ public class UDPManager extends Thread {
             DatagramPacket datagramPacketToBeSent = new DatagramPacket(data, data.length);
             datagramPacketToBeSent.setAddress(InetAddress.getByName(this.defaultIpAddress));
             datagramPacketToBeSent.setPort(this.listeningPort);
-            datagramSocket.send(datagramPacketToBeSent);
+            multicastSocket.send(datagramPacketToBeSent);
             return true;
         } catch (IOException error) {
             this.caller.exceptionHasBeenThrown(error);
