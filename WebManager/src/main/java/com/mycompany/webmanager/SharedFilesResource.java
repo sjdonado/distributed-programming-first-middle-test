ab64901d302dc22e062f98e65ba1a7bd0cef985b
Brian Ramirez <brians@uninorte.edu.co>
2019-09-15 00:02:39 -0500

    feat: Working on Web Manager implementation   - Gateway flow implemented using one web service Related to: #2

---
+++ WebManager/src/main/java/com/mycompany/webmanager/SharedFilesResource.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.webmanager;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import org.apache.commons.io.FileUtils;
+
+/**
+ * REST Web Service
+ *
+ * @author brian
+ */
+@Path("shared_files")
+public class SharedFilesResource {
+    
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of SharedFilesResource
+     */
+    public SharedFilesResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.mycompany.webmanager.SharedFilesResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String index() {
+        try{
+            String endpoint="http://localhost:8080/WebService/webresources/files";
+            String methodType="GET";
+            URL url = new URL(endpoint);
+            HttpURLConnection urlConnection=(HttpURLConnection)url.openConnection();
+            urlConnection.setRequestMethod(methodType);
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-type", MediaType.APPLICATION_JSON);
+            urlConnection.setRequestProperty("Accept", MediaType.APPLICATION_JSON);
+            int httpResponseCode = urlConnection.getResponseCode();
+            if (httpResponseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                return content.toString();
+            }
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+        return null;
+    }
+    
+     /**
+     * Retrieves representation of an instance of com.mycompany.webservice.FilesResource
+     * @param filename
+     * @return an instance of java.io.File;
+     */
+    @GET
+    @Path("{filename}")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public File show(@PathParam("filename") String filename) {
+        try{
+            String endpoint = "http://localhost:8080/WebService/webresources/files";
+            URL url = new URL(endpoint + "/" + filename);
+            File file = new File(filename);
+            FileUtils.copyURLToFile(url, file);
+            return file;
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+        return null;
+    }
+}
