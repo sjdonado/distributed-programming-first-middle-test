e54ab192f94700cb7c49ebd43efd5b72fae3f406
Brian Ramirez <brians@uninorte.edu.co>
2019-09-18 12:06:22 -0500

    feat: Read web service initializer variables from file Resolves: #6

--- WebService/src/main/java/com/mycompany/webservice/InitializerSetting.java
+++ WebService/src/main/java/com/mycompany/webservice/InitializerSetting.java
@@ -5,11 +5,17 @@
  */
 package com.mycompany.webservice;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -22,18 +28,45 @@ import javax.ws.rs.core.MediaType;
 @Singleton
 @Startup
 public class InitializerSetting {
-    final static String DEFAULT_PORT = "8080";
+    static final String DEFAULT_PORT = "8080";
+    static final Map<String,String> SERVER_SETTING_VARS 
+        = new HashMap<>();  
+    static final String SERVER_CONFIG_FILE_PATH = System
+        .getProperty("catalina.base") + File.separator + "config"
+        + File.separator + "web-service-setting.txt";
+    
     @PostConstruct
     public void onStartup() {
-        this.connectToWebManagement();
+        try {
+            this.readEnvVariables();
+            this.connectToWebManagement();
+        } catch (Exception e) {
+            System.err.println(
+                "Initializing web service error: " + e.getMessage()
+            );
+        }
     }
+    
+    private void readEnvVariables() throws FileNotFoundException, IOException {
+        File settingFile = new File(SERVER_CONFIG_FILE_PATH);
+        BufferedReader br = new BufferedReader(new FileReader(settingFile));
+        String line;
+        while ((line = br.readLine()) != null) {
+          String[] config = line.split("=");
+          SERVER_SETTING_VARS.put(config[0], config [1]);
+        }
+    }   
     
     private void connectToWebManagement() {
         try{
             String ip = InetAddress.getLocalHost().getHostAddress();
-            String address = ip + ":" + DEFAULT_PORT; 
-            String endpoint = "http://127.0.0.1:8080/WebManager/webresources/connection";
+            String address = ip + ":"
+                + SERVER_SETTING_VARS.get("WEB_SERVICE_PORT");
+            String endpoint = "http://"
+                + SERVER_SETTING_VARS.get("WEB_MANAGER_ADDRESS")
+                + "/WebManager/webresources/connection";
             String methodType = "POST";
+            System.out.println("ADDRESS " + endpoint);
             URL url = new URL(endpoint);
             HttpURLConnection urlConnection = (HttpURLConnection)
                 url.openConnection();
