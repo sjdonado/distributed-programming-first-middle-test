3c3c48a62a9b01a6c92e74bc9d2ec8bd4c55bd45
Brian Ramirez <brians@uninorte.edu.co>
2019-09-26 21:14:08 -0500

    feat: Sync webservices file dir on startup

--- WebManager/src/main/java/com/mycompany/webmanager/ConnectionManagement.java
+++ WebManager/src/main/java/com/mycompany/webmanager/ConnectionManagement.java
@@ -5,7 +5,29 @@
  */
 package com.mycompany.webmanager;
 
+import com.google.gson.Gson;
+import java.io.File;
 import java.util.ArrayList;
+import com.mycompany.webmanagerclient.SharedFile;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 /**
  *
@@ -51,5 +73,103 @@ public class ConnectionManagement {
     
     public ArrayList<String> getServers() {
         return servers;
+    }
+    
+    public void syncWebServiceFiles(String sv) {
+        Map<String, String> serverFiles = new HashMap<>();
+        this.servers.forEach(server -> {
+            if (!server.equals(sv)) {
+                SharedFile[] fls = this.fetchServerFileList(server);
+                for(SharedFile sharedFile : fls) {
+                    String filename = sharedFile.getName();
+                    if (!checkIfFileExists(serverFiles.values(), filename)) {
+                        serverFiles.put(server, filename);
+                        InputStream fileStream;
+                        try {
+                            File fl = this.downloadFile(server, filename);
+                            this.uploadFile(sv, filename, fl);
+                        } catch (IOException ex) {
+                            Logger.getLogger(ConnectionManagement.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            }
+        });
+    }
+    
+    private SharedFile[] fetchServerFileList(String address) {
+        try{
+            String methodType = "GET";
+            URL url = new URL("http://" + address + "/WebService/webresources/files");
+            HttpURLConnection urlConnection= (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod(methodType);
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-type", "application/json");
+            urlConnection.setRequestProperty("Accept", "application/json");
+            int httpResponseCode = urlConnection.getResponseCode();
+            if (httpResponseCode == HttpURLConnection.HTTP_OK) {
+                Gson gson = new Gson();
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(urlConnection.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                return gson.fromJson(content.toString(), SharedFile[].class);
+            }
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+        return null;
+    }
+    
+    private boolean checkIfFileExists(Collection<String> currentFiles, String filename) {
+        return currentFiles.stream().anyMatch(
+            (fname) -> (fname.equals(filename))
+        );
+    }
+    
+    private File downloadFile(String address, String filename) throws IOException {
+        String endpoint = "http://" + address
+            + "/WebService/webresources/files";
+        URL url = new URL(endpoint + "/" + filename);
+        String methodType = "GET";
+        HttpURLConnection urlConnection = (HttpURLConnection) 
+            url.openConnection();
+        urlConnection.setRequestMethod(methodType);
+        urlConnection.setDoOutput(true);
+        urlConnection.setRequestProperty(
+            "Content-type", MediaType.APPLICATION_OCTET_STREAM
+        );
+        urlConnection.setRequestProperty(
+            "Accept", MediaType.APPLICATION_OCTET_STREAM
+        );
+        InputStream t = urlConnection.getInputStream();
+        File file = new File(filename);
+        FileUtils.copyURLToFile(url, file);
+        return file;
+    }
+    
+    private void uploadFile(String address, String filename, File file) throws IOException {
+        String endpoint = "http://" + address
+            + "/WebService/webresources/files/upload";
+        URL url = new URL(endpoint + "/" + filename);
+        String methodType = "POST";
+        HttpURLConnection urlConnection = (HttpURLConnection) 
+            url.openConnection();
+        urlConnection.setRequestMethod(methodType);
+        urlConnection.setDoOutput(true);
+        urlConnection.setRequestProperty(
+            "Content-type", MediaType.APPLICATION_OCTET_STREAM
+        );
+        urlConnection.setRequestProperty(
+            "Accept", MediaType.APPLICATION_OCTET_STREAM
+        );
+        OutputStream os = urlConnection.getOutputStream();
+        IOUtils.copy(new FileInputStream(file),os);
+        System.out.println(
+            "Upload file to Web Service => " + urlConnection.getResponseCode()
+        );
     }
 }
